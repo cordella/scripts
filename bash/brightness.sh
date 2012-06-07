929d5ae5e03764859d20365f1628b60d72789b59
Joseph Leeper <cordella@gmx.com>
2012-06-07 04:59:31 -0500

    Updated description.

--- bash/brightness.sh
+++ bash/brightness.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+#
+# Changes the brightness of the specified screen.
+# Your specific path will likely vary, so customise to your needs.
+
 echo $1 > /sys/devices/pci0000:00/0000:00:0b.0/0000:02:00.0/backlight/acpi_video0/brightness
 
