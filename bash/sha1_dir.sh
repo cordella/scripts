44ffcca7ed5c3ed126a5c678df0b4e645ab7bc99
Joseph Leeper <cordella@gmx.com>
2012-06-07 03:58:21 -0500

    Description change.

--- bash/sha1_dir.sh
+++ bash/sha1_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Makes a file.sha1 with the output from sha1sum for each file in
+# Outputs to stdout the output from sha1sum for each file in
 # the directory specified on the command line.
 
 EWD=$1
