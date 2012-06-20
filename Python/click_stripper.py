39d0818dea48fe36d512fbe3986461ddf5d9a8cb
Joseph Leeper <cordella@gmx.com>
2012-06-20 08:34:22 -0500

    Improved the structure and functionality.

--- Python/click_stripper.py
+++ Python/click_stripper.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
-from re import findall
 from sys import version_info, argv, exit
+
+if len(argv) < 2:
+    print('must provide at least one URL')
+    exit()
+
+from re import findall
+
+# Version agnosticism
 if (version_info[0] <= 2):
     from urllib import unquote
 else:
     from urllib.parse import unquote
 
-try:
-    url = findall('(?iu)https?%3A%2F%2F.*?(?=&|\\Z|\\s|\\?)', argv[1])[0]
-except IndexError:
-    print('incompatible entry')
-    exit()
+urls = []
 
-print(unquote(url))
+""" Attempt to extract real URLs from click-tracked URLs. """
+for opt in argv[1:]:
+    # This regular expression extracts encoded URLs
+    # (ones that start with http%3A%2F%2F instead of http://)
+    urls.append(findall('(?iu)https?%3A%2F%2F.*?(?=&|\\Z|\\s|\\?)', opt))
+
+# Reduces urls to a list of strings.
+urls = [i for l in urls for i in l]
+
+print()
+for url in urls:
+    print(unquote(url))
 
